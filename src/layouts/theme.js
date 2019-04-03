f5869be5616d371b0ea452e189d311646dfabd66
Alec Lomas <alec@lowmess.com>
2019-04-02 22:14:04 -0700

    :lipstick: update color palette
    to make accessible buttons among other more neurotic reasons

--- src/layouts/theme.js
+++ src/layouts/theme.js
@@ -1,19 +1,66 @@
 const breakpoints = ['48em', '64em']
 
+// more colors at https://palx.jxnblk.com/15ab3d.json
 const colors = {
-  green: '#4DB85C',
-  greens: ['#C5E1B4', '#93C774', '#4DB85C', '#5B913D', '#4C7832'],
+  green: '#15ab3d',
+  greens: [
+    '#e4f5e9',
+    '#c8ebd1',
+    '#a7dfb6',
+    '#81d297',
+    '#53c170',
+    '#15ab3d',
+    '#129a37',
+    '#108730',
+    '#0d7027',
+    '#09501c',
+  ],
 
-  red: '#EF1F26',
-  reds: ['#FABAC8', '#F4718D', '#EF1F26', '#D81C44', '#9D1432'],
+  red: '#ab3d15',
+  reds: [
+    '#f6ebe7',
+    '#edd6cd',
+    '#e2bdb0',
+    '#d5a08c',
+    '#c57a5e',
+    '#ab3d15',
+    '#993612',
+    '#862f10',
+    '#6e270d',
+    '#4d1b09',
+  ],
 
-  yellow: '#EFC21F',
-  yellows: ['#F8E49F', '#F2CF4F', '#EFC21F', '#D8AF1C', '#9D8014'],
+  // yellows not pulled from same palette as other shades
+  yellow: '#efc21f',
+  yellows: [
+    '#fcf6e1',
+    '#faeec1',
+    '#f8e49f',
+    '#f5da79',
+    '#f2cf4f',
+    '#efc21f',
+    '#d8af1c',
+    '#be9a18',
+    '#9d8014',
+    '#725c0e',
+  ],
 
-  black: '#344236',
-  grey: '#A3B8A6',
-  white: '#FFFFFF',
-  greys: ['#FFFFFF', '#EAEEEA', '#CAD5CB', '#A3B8A6', '#719075', '#344236'],
+  white: '#ffffff',
+  gray: '#bcc6bf',
+  black: '#334237',
+  grays: [
+    '#ffffff',
+    '#f8f9f8',
+    '#ebeeec',
+    '#dde1de',
+    '#cdd4cf',
+    '#bcc6bf',
+    '#aab5ad',
+    '#94a398',
+    '#7b8d7f',
+    '#5a7160',
+    '#334237',
+  ],
 }
 
 // Box-sizing & borders
@@ -75,7 +122,7 @@ const buttons = {
     transition: 'background-color 0.2s ease',
 
     '&:hover': {
-      backgroundColor: colors.greens[3],
+      backgroundColor: colors.greens[6],
     },
   },
 }
