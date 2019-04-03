b36a5c2d2df7cf5fec8225dba668e2a0bdd4048c
Alec Lomas <alec@lowmess.com>
2019-04-03 13:28:45 -0700

    :art: move Hero into nested folder
    so I can have the logo be it's own thing

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,9 @@ import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import { css } from 'styled-components'
 import { Box, Button } from 'rebass'
-import Container from '../components/Container'
-import MarkdownContent from '../components/MarkdownContent'
+import Container from '../Container'
+import MarkdownContent from '../MarkdownContent'
+import Logo from './Logo'
 
 const Hero = () => {
   const { contentfulHomePageHero: data } = useStaticQuery(graphql`
@@ -44,7 +45,7 @@ const Hero = () => {
       position: absolute;
       top: 0;
       right: 0;
-      bottom 0;
+      bottom: 0;
       left: 0;
       width: 100%;
       height: 100%;
@@ -72,20 +73,11 @@ const Hero = () => {
         css={imageStyles}
       />
 
-      <Box mb={5} color="white" css="position: relative; text-align: center;">
-        <svg
-          viewBox="0 0 241 190"
-          xmlns="http://www.w3.org/2000/svg"
-          width="128"
-          height="96"
-        >
-          <path
-            fillRule="evenodd"
-            d="M203.972 1.608C107.648-9.962 97.001 45.208.006 30.704v156.964c101.492 11.108 116.228-38.628 203.966-29.095V1.608zM47.922 142.382c5.094 2.419 12.88 4.25 20.426 3.783 16.131-1 29.027-8.423 27.76-28.851l-2.964-47.875-13.616.843.355 5.762c-1.701-1.998-7.565-6.683-16.782-6.112-15.609.967-25.975 13.597-24.93 30.464 1.041 16.866 12.147 28.062 27.968 27.082 9.532-.59 14.538-6.369 15.593-8.012l.153 2.514c.528 8.486-6.554 11.343-14.41 11.83-7.54.467-14-1.972-15.84-2.805l-3.714 11.377zm61.605-19.002c5.015 2.57 12.748 4.634 20.303 4.393 16.156-.516 29.27-7.55 28.613-28.008l-1.528-47.942-13.641.435.187 5.77c-1.642-2.047-7.363-6.905-16.595-6.61-15.633.499-26.369 12.813-25.831 29.703.542 16.89 11.303 28.413 27.148 27.907 9.543-.305 14.721-5.93 15.826-7.541l.079 2.518c.27 8.497-6.89 11.141-14.76 11.392-7.551.241-13.937-2.39-15.752-3.277l-4.05 11.26zM80.8 97.65c.602 9.743-4.665 16.379-13.044 16.898-8.171.506-14.435-5.521-15.031-15.159-.597-9.638 4.773-16.28 12.945-16.786 8.487-.526 14.553 5.724 15.13 15.047zm62.932-17.997c.311 9.756-5.153 16.231-13.547 16.5-8.181.26-14.262-5.95-14.568-15.603-.31-9.651 5.258-16.13 13.444-16.39 8.497-.272 14.37 6.156 14.671 15.493z"
-          />
-          <path d="M216.304 1.623h24.676V158.6h-24.676z" />
-        </svg>
-      </Box>
+      <Logo
+        mb={5}
+        color="white"
+        css="position: relative; text-align: center;"
+      />
 
       <Box css={contentStyles}>
         <Container
