be178e943a7a56b54ecf12d9d89a9fc8724d19fa
Alec Lomas <alec@lowmess.com>
2019-04-17 15:05:32 -0700

    :bug: prevent wave from separating from footer

--- src/components/Footer/Wave.js
+++ src/components/Footer/Wave.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from 'rebass'
 
 const FlagAbove = props => (
-  <Box css="position: relative; top: 2px;" {...props}>
+  <Box css="position: relative; top: 4px;" {...props}>
     <svg viewBox="0 0 1024 48" xmlns="http://www.w3.org/2000/svg">
       <path d="M0 6.67c392.902 80.215 640.91-47.562 1024 7.679v33.427H0V6.67z" />
     </svg>
