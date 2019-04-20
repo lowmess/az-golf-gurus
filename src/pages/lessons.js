dd0062d71ca323a9ee48c5148e9ea91cd84ab2c0
Alec Lomas <alec@lowmess.com>
2019-04-19 21:54:37 -0700

    :lipstick: style lessons page

--- src/pages/lessons.js
+++ src/pages/lessons.js
@@ -1,14 +1,44 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import { Box } from 'rebass'
-import { Heading } from '../components/Typography'
+import styled from 'styled-components'
+import { Box, Flex, Text, Button } from 'rebass'
+import { Heading, Paragraph } from '../components/Typography'
 import Container from '../components/Container'
-import { Header, HeaderTitle } from '../components/Header'
+import { Header, HeaderTitle, HeaderDescription } from '../components/Header'
 import { toMoney } from '../utils/price'
+import { themeHover } from '../utils/styles'
+import unwidow from '../utils/unwidow'
+
+const LessonGroup = styled(Box)`
+  & + & {
+    margin-top: ${({ theme }) => theme.space[5]};
+    border-top: ${({ theme }) => `${theme.borders[1]} ${theme.colors.green}`};
+    padding-top: ${({ theme }) => theme.space[5]};
+  }
+`
+
+const Lesson = styled(Box)`
+  & + & {
+    margin-top: ${({ theme }) => theme.space[5]};
+
+    @media (min-width: ${({ theme }) => theme.breakpoints[0]}) {
+      margin-top: 0;
+      margin-left: ${({ theme }) => theme.space[6]};
+    }
+  }
+`
 
 const LessonsPage = () => {
   const data = useStaticQuery(graphql`
     query {
+      contentfulLessonsPage(title: { ne: "SCHEMA__LessonsPage" }) {
+        title
+        description {
+          content: childMarkdownRemark {
+            html
+          }
+        }
+      }
       allContentfulLessonCategory(
         filter: { title: { ne: "SCHEMA__LessonCategory" } }
         sort: { order: ASC, fields: [order] }
@@ -27,36 +57,72 @@ const LessonsPage = () => {
     }
   `)
 
+  const pageTitle =
+    (data.contentfulLessonsPage && data.contentfulLessonsPage.title) ||
+    'Lessons'
+  const hasDescription =
+    data.contentfulLessonsPage && data.contentfulLessonsPage.description
+
   return (
     <>
       <Header>
-        <HeaderTitle>Lessons</HeaderTitle>
+        <HeaderTitle>{pageTitle}</HeaderTitle>
+
+        {hasDescription && (
+          <HeaderDescription markdown>
+            {data.contentfulLessonsPage.description.content.html}
+          </HeaderDescription>
+        )}
       </Header>
 
-      <Container>
-        <Box>
-          {data.allContentfulLessonCategory.edges.map(edge => {
-            const { title, lessons } = edge.node
+      <Container my={5} css="text-align: center;">
+        {data.allContentfulLessonCategory.edges.map(edge => {
+          const { title, lessons } = edge.node
 
-            return (
-              <Fragment key={edge.node.title}>
-                <Heading>{title}</Heading>
+          return (
+            <LessonGroup key={edge.node.title}>
+              <Heading mb={[5, 4]} fontSize={[5, 6]}>
+                {unwidow(title)}
+              </Heading>
 
-                <Box>
-                  {lessons.map(lesson => {
-                    return (
-                      <p key={lesson.contentful_id}>
-                        <Link to={`/lessons/${lesson.contentful_id}/`}>
-                          {lesson.title} - {toMoney(lesson.price)}
+              <Flex
+                flexDirection={['column', 'row']}
+                justifyContent="center"
+                alignItems="center"
+              >
+                {lessons.map(lesson => {
+                  return (
+                    <Lesson key={lesson.contentful_id}>
+                      <Paragraph mt={0} mx="auto" mb={3} fontSize={[3, 4]}>
+                        <Link
+                          to={`/lessons/${lesson.contentful_id}/`}
+                          css={themeHover}
+                        >
+                          <Text
+                            as="span"
+                            fontFamily="geomanist"
+                            fontWeight="bold"
+                          >
+                            {unwidow(lesson.title)}
+                          </Text>{' '}
+                          {'\u2022'} {toMoney(lesson.price)}
                         </Link>
-                      </p>
-                    )
-                  })}
-                </Box>
-              </Fragment>
-            )
-          })}
-        </Box>
+                      </Paragraph>
+
+                      <Button
+                        variant="outline"
+                        as={Link}
+                        to={`/lessons/${lesson.contentful_id}/`}
+                      >
+                        Book
+                      </Button>
+                    </Lesson>
+                  )
+                })}
+              </Flex>
+            </LessonGroup>
+          )
+        })}
       </Container>
     </>
   )
