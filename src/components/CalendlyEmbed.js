77b40a2e7c00507db93733fd31328453cc29b77b
Alec Lomas <alec@lowmess.com>
2019-04-19 20:32:26 -0700

    :recycle: move calendly embed to own component
    also only load the script once

---
+++ src/components/CalendlyEmbed.js
@@ -0,0 +1,57 @@
+/* global Calendly */
+
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Box, Card } from 'rebass'
+import { Paragraph } from './Typography'
+import { useScript } from '../utils/hooks'
+
+const CalendlyEmbed = ({ url, ...props }) => {
+  const [loaded, error] = useScript(
+    'https://assets.calendly.com/assets/external/widget.js'
+  )
+
+  useEffect(() => {
+    // I got this method by running Prettier on `widget.js`.
+    // If it stops working it's likely the method has changed.
+    if (loaded && !error && typeof Calendly !== 'undefined') {
+      Calendly._autoLoadInlineWidgets()
+    }
+  }, [loaded, error])
+
+  if (error) {
+    return (
+      <Card
+        bg="reds.0"
+        border="1"
+        borderColor="reds.3"
+        borderRadius="2"
+        py={2}
+        px={3}
+        color="reds.8"
+        css="text-align: center;"
+        {...props}
+      >
+        <Paragraph mx="auto">
+          There was an error loading the scheduling widget. Please try
+          again&nbsp;later.
+        </Paragraph>
+      </Card>
+    )
+  }
+
+  return (
+    <Box
+      className="calendly-inline-widget"
+      data-url={url}
+      style={{ minWidth: '20rem', height: '40rem' }}
+      {...props}
+    />
+  )
+}
+
+CalendlyEmbed.propTypes = {
+  url: PropTypes.string.isRequired,
+}
+
+export default CalendlyEmbed
--- src/templates/Event.js
+++ src/templates/Event.js
@@ -1,27 +1,16 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import { Box } from 'rebass'
 import Container from '../components/Container'
 import { Header, HeaderTitle, HeaderDescription } from '../components/Header'
+import CalendlyEmbed from '../components/CalendlyEmbed'
 import { toMoney } from '../utils/price'
 import unwidow from '../utils/unwidow'
 
 const EventTemplate = ({ data }) => {
   const event = data.contentfulEvent
   const description = event.description && event.description.content.html
-
-  useEffect(() => {
-    if (typeof document === 'undefined') return false
-
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = 'https://assets.calendly.com/assets/external/widget.js'
-    script.async = true
-    // sort of unfortunate there's no way to force Calendly to re-mount,
-    // so we have to append it each time
-    document.body.appendChild(script)
-  }, [])
 
   return (
     <Container>
@@ -31,7 +20,7 @@ const EventTemplate = ({ data }) => {
           {'\u2014'} <time dateTime={event.endDate}>{event.endDateString}</time>
         </Box>
 
-        <HeaderTitle>{event.title}</HeaderTitle>
+        <HeaderTitle>{unwidow(event.title)}</HeaderTitle>
 
         <Box as="p" mt={3} fontSize={[1, 2, 3]}>
           {unwidow(event.location)} {'\u2022'} {toMoney(event.price)}
@@ -44,13 +33,7 @@ const EventTemplate = ({ data }) => {
         )}
       </Header>
 
-      <Box
-        mt={6}
-        mb={4}
-        className="calendly-inline-widget"
-        data-url={event.calendlyUrl}
-        style={{ minWidth: '320px', height: '1024px' }}
-      />
+      <CalendlyEmbed url={event.calendlyUrl} mt={[5, 6]} mb={5} />
     </Container>
   )
 }
