a104342fa912de3110f4ceeae6ea9c267362472b
Alec Lomas <alec@lowmess.com>
2019-11-23 17:51:59 -0700

    :lipstick: add events page empty state

--- src/pages/events.js
+++ src/pages/events.js
@@ -6,6 +6,7 @@ import { Text, Heading, Link, Button } from 'rebass'
 import List from '../components/List'
 import Container from '../components/Container'
 import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
 import { useSiteMetadata } from '../utils/hooks'
 import { toMoney } from '../utils/price'
 import unwidow from '../utils/unwidow'
@@ -18,6 +19,8 @@ const EventsPage = ({ data }) => {
 
   const hasDescription =
     data.contentfulEventsPage && data.contentfulEventsPage.description
+
+  const hasEvents = data.allContentfulEvent.edges.length > 0
 
   return (
     <>
@@ -37,66 +40,74 @@ const EventsPage = ({ data }) => {
         )}
       </Header>
 
-      <Container>
-        <List textAlign="center" py={5}>
-          {data.allContentfulEvent.edges.map(edge => {
-            const {
-              contentful_id,
-              title,
-              price,
-              location,
-              startDate,
-              startDateString,
-              endDate,
-              endDateString,
-            } = edge.node
+      <Container py={5} textAlign="center">
+        {!hasEvents && (
+          <Paragraph mx="auto">
+            There are no upcoming events. Check back soon!
+          </Paragraph>
+        )}
 
-            const url = `/events/${contentful_id}/`
+        {hasEvents && (
+          <List>
+            {data.allContentfulEvent.edges.map(edge => {
+              const {
+                contentful_id,
+                title,
+                price,
+                location,
+                startDate,
+                startDateString,
+                endDate,
+                endDateString,
+              } = edge.node
 
-            return (
-              <List.Item
-                sx={{
-                  '& + &': {
-                    marginTop: 5,
-                    borderTop: 1,
-                    borderColor: 'green',
-                    paddingTop: 5,
-                  },
-                }}
-                key={contentful_id}
-              >
-                <Text
-                  as="p"
-                  m={0}
-                  fontSize={[1, 2]}
-                  sx={{ fontStyle: 'italic' }}
+              const url = `/events/${contentful_id}/`
+
+              return (
+                <List.Item
+                  sx={{
+                    '& + &': {
+                      marginTop: 5,
+                      borderTop: 1,
+                      borderColor: 'green',
+                      paddingTop: 5,
+                    },
+                  }}
+                  key={contentful_id}
                 >
-                  <time dateTime={startDate}>{startDateString}</time> {'\u2014'}{' '}
-                  <time dateTime={endDate}>{endDateString}</time>
-                </Text>
+                  <Text
+                    as="p"
+                    m={0}
+                    fontSize={[1, 2]}
+                    sx={{ fontStyle: 'italic' }}
+                  >
+                    <time dateTime={startDate}>{startDateString}</time>{' '}
+                    {'\u2014'} <time dateTime={endDate}>{endDateString}</time>
+                  </Text>
 
-                <Heading fontSize={[3, 4, 5]} fontWeight="bold">
-                  <Link variant="ui-link" as={GatsbyLink} to={url}>
-                    {unwidow(title)}
-                  </Link>
-                </Heading>
+                  <Heading fontSize={[3, 4, 5]} fontWeight="bold">
+                    <Link variant="ui-link" as={GatsbyLink} to={url}>
+                      {unwidow(title)}
+                    </Link>
+                  </Heading>
 
-                <Text as="p" mt={3} mb={4} fontSize={[1, 2, 3]}>
-                  {unwidow(location)} {'\u2022'} {toMoney(price)}
-                </Text>
+                  <Text as="p" mt={3} mb={4} fontSize={[1, 2, 3]}>
+                    {unwidow(location)} {'\u2022'} {toMoney(price)}
+                  </Text>
 
-                <Button
-                  variant="outline"
-                  as={GatsbyLink}
-                  to={url}
-                  fontSize={[1, 2]}
-                >
-                  Register
-                </Button>
-              </List.Item>
-            )
-          })}
-        </List>
+                  <Button
+                    variant="outline"
+                    as={GatsbyLink}
+                    to={url}
+                    fontSize={[1, 2]}
+                  >
+                    Register
+                  </Button>
+                </List.Item>
+              )
+            })}
+          </List>
+        )}
       </Container>
     </>
   )
