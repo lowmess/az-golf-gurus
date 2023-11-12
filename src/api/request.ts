84092bed7c42f424e2a84caa541356999ecffca5
Alec Lomas <alec@lowmess.com>
2023-11-12 03:06:14 -0800

    :bug: only set X-Include-Drafts in dev

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,12 +1,17 @@
 export async function request(query: string) {
+	const headers: Record<string, string> = {
+		"Content-Type": "application/json",
+		Accept: "application/json",
+		Authorization: `Bearer ${import.meta.env.DATOCMS_READ_ONLY_TOKEN}`,
+	};
+
+	if (import.meta.env.DEV) {
+		headers["X-Include-Drafts"] = "true";
+	}
+
 	const res = await fetch("https://graphql.datocms.com/", {
 		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Accept: "application/json",
-			Authorization: `Bearer ${import.meta.env.DATOCMS_READ_ONLY_TOKEN}`,
-			"X-Include-Drafts": import.meta.env.DEV.toString(),
-		},
+		headers,
 		body: JSON.stringify({
 			query,
 		}),
